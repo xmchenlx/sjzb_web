7e24b1197c6ba57bede480cd1a98390a6f11dd77
xmchenlx <44153332+xmchenlx@users.noreply.github.com>
2020-03-18 17:38:20 +0800

    修改axios接口为https

--- start.sh
+++ start.sh
@@ -1,13 +1,21 @@
 git pull
 
+#停止Docker里的nginx（name）
+docker stop brps_web_nginx
+
 #删除容器
-docker rm -f brps_web_nginx &> /dev/null
+docker rm -f brps_nginx &> /dev/null
+
+#关闭docker
+sudo systemctl stop docker
 
 #build依赖
 npm config set registry http://registry.cnpmjs.org
 
 npm run build
 
+#打开docker
+sudo systemctl start docker
 
 #启动容器
-docker run --name brps_web_nginx -d -p 8081:80 -v $PWD/dist:/usr/share/nginx/html nginx
+docker run --name brps_web_nginx -d -p 8081:80 -p 443:443 -v $PWD/dist:/usr/share/nginx/html nginx -v /root/nginx-1.8.1/conf/nginx.conf:/usr/share/nginx.conf
